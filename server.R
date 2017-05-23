ab413c2d331ce2d77992d1bb6902a9cd34cb89d3
Utako Kase <utakok@uw.edu>
2017-05-23 14:53:01 -0700

    instruction added to server.R and ui.R as comment

--- server.R
+++ server.R
@@ -5,6 +5,14 @@ setwd("~/Desktop/INFO201/Info201GroupProj")
 shinyServer(function(input, output){
   movie.data <- read.csv("bechdel_data/movies.csv")
   source("script/find_movie.R")
-  #source("script/get_num_in_year.R")
+  #source("script/get_num_in_year.R") <--somehow this doesn't work...
+  
+  
+  
+  #YOUR SHOULD MODIFY THIS FILE BY ADDINT SOMETHING LIKE THIS???
+  #output$pg1 <- renderPlot({ 
+  #  return(YOUR.FUNCTION.NAME(movie.data, OTHER.PARAMETER))
+  #})
+  
   
 })
--- ui.R
+++ ui.R
@@ -4,4 +4,8 @@ shinyUI(shinyUI(navbarPage("Bechdel Test",
                            tabPanel("Page 2"),
                            tabPanel("Page 3"),
                            tabPanel("page 4")
+                           
+                           #YOU SHOULD MODIFY THIS FILE BY ADDING YOUR OUTPUT AFTER THE PAGE NAME LIKE THIS:
+                           #tabPanel("Page 1", plotOutput("pg1")) <--I think.....? 
+                           
 )))
