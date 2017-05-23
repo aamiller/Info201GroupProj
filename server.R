98c81f246e68eafa892dc36332fe0e18ecb53394
Utako Kase <utakok@uw.edu>
2017-05-23 14:05:39 -0700

    4 tabs created in server.R

--- server.R
+++ server.R
@@ -1,8 +1,11 @@
 library(shiny)
 library(dplyr)
-
+setwd("~/Desktop/INFO201/Info201GroupProj")
 
 shinyServer(function(input, output){
- 
+  movie.data <- read.csv("bechdel_data/movies.csv")
+  source("script/find_movie.R")
+  #source("script/get_num_in_year.R") <--somehow this doesn't work...
+  
   
 })
--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 library(plotly)
-shinyUI(fluidPage(
-  
-  
-))
+shinyUI(shinyUI(navbarPage("Bechdel Test",
+                           tabPanel("Page 1"),
+                           tabPanel("Page 2"),
+                           tabPanel("Page 3"),
+                           tabPanel("page 4")
+)))
