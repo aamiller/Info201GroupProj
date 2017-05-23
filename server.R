8bde8de9409ad36ee2f7a4b13d0dd242391a53ad
Utako Kase <utakok@uw.edu>
2017-05-23 14:30:17 -0700

    added 2 sources

--- server.R
+++ server.R
@@ -5,7 +5,6 @@ setwd("~/Desktop/INFO201/Info201GroupProj")
 shinyServer(function(input, output){
   movie.data <- read.csv("bechdel_data/movies.csv")
   source("script/find_movie.R")
-  #source("script/get_num_in_year.R") <--somehow this doesn't work...
-  
+  source("script/get_num_in_year.R")
   
 })
