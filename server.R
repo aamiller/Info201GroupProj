216bf2af07412ee2f26d47e4fe4472c2092de58c
Anni Yan <Anni Yan>
2017-05-31 16:32:20 -0700

    added server comment

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+#loading required libraries
 library(shiny)
 library(dplyr)
 library(scales)
@@ -5,35 +6,43 @@ library(ggplot2)
 library(plotly)
 library(knitr)
 
-#setwd("~/Desktop/INFO201/Info201GroupProj")
-movie.data <- read.csv("./bechdel_data/movies.csv", stringsAsFactors =  FALSE)
-joined.movie.data <- read.csv("./bechdel_data/final_joined_bechdel_data.csv", stringsAsFactors = FALSE)
+#loading required sources
 source("./script/profitBechdelAssessment.R")
 source("./script/genreAnalysisGraph.R")
 source('./script/year.R')
 source("./script/ratingBarGraph.R")
 source("./script/country.graph.R")
-shinyServer(function(input, output) {
 
-  output$profitBechdelAssessment <- renderPlotly({
-    return(BuildScatter(movie.data, input$scatterVarX, input$scatterVarY))
-  })
+#making useful dataframes
+movie.data <- read.csv("./bechdel_data/movies.csv", stringsAsFactors =  FALSE)
+joined.movie.data <- read.csv("./bechdel_data/final_joined_bechdel_data.csv", stringsAsFactors = FALSE)
+
+#making a server with input and output
+shinyServer(function(input, output) {
   
+  #returns a graph that shows adjust rating affect the passing rate
   output$contentRatingBechdelAssessment <- renderPlotly({
     return(BuildContentRatingBarGraph(joined.movie.data))
   })
   
+  #returns a scatter plot of budget affect the passing rate
+  output$profitBechdelAssessment <- renderPlotly({
+    return(BuildScatter(movie.data, input$scatterVarX, input$scatterVarY))
+  })
+  
+  #returns a line graph of years affect the passing rate
   output$linegraph <- renderPlotly ({
     modified_data <- year(movie.data)
     modified_data <- modified_data %>% 
       filter(year >= input$slider[1] & year <= input$slider[2]) %>% 
       mutate(pass_ratio = PASS / total)
-  
+    
     plot_ly(x = modified_data$year, y = (modified_data[, input$button] / modified_data$total), 
-                 type = "scatter", mode = "lines", color = modified_data$pass_ratio, 
-                 name = input$button) 
+            type = "scatter", mode = "lines", color = modified_data$pass_ratio, 
+            name = input$button) 
   })
   
+  #returns a dot graph of test results and years
   output$testResults <- renderPlotly({
     group_test <- joined.movie.data %>% 
       filter(binary == input$button) %>% 
@@ -44,23 +53,26 @@ shinyServer(function(input, output) {
     ggplotly(g)
   })
   
+  #returns a barplot of genre affect the passing rate
   output$GenreBechdelAssessmentBar <- renderPlotly({
     BuildGenreBarPlot(joined.movie.data)
   })
   
+  #returns a plot??
   output$main_plot <- renderPlot({    
     plot(x, y)}, height = 200, width = 300)
-
   
+  #returns a plot??
   output$budgetText <- renderText({
     return("EDIT YOUR TEXT/CONCLUTION HERE FOR BUDGE PAGE")
   })
-
   
+  #returns a map of countries affect the passing rate
   output$countryGraph <- renderPlotly({
     return(countryGraph(joined.movie.data))
   })
   
+  #return a table of a dataset
   output$movieInfo <- renderDataTable({
     movie_title <- joined.movie.data %>%
       select(title, imdb, year, clean_test, binary, genres,
