80a36e089dfc914d1a5e9393625a30ab9f8f890b
Keenan Goodman <goodman3@Keenans-MacBook-Pro.local>
2017-05-31 18:49:39 -0700

    added comments

--- script/country.graph.R
+++ script/country.graph.R
@@ -5,19 +5,26 @@ library(plotly)
 
 
 
-
+# this function outputs a choropleth world map that shows some data relating different companies
 countryGraph <- function(country.movie.data) {
+  # the next few lines of code clean up the data and make a new data frame out of it only including the
+  # information needed for the plot.
 country.movie.data <- country.movie.data %>% group_by(country) %>% summarise("number.passed" = sum(binary == "PASS"),
                                                                  "number.failed" = sum(binary == "FAIL"))
 country.movie.data <- mutate(country.movie.data, "passage.rate" = number.passed/(number.passed + number.failed))
 country.movie.data <- mutate(country.movie.data, "number.movies" = number.passed + number.failed)                
 country.movie.data <- country.movie.data[-c(22), ] #remove "official site", cannot be mapped as location, not a location.
 country.movie.data <- country.movie.data[-c(31), ] #remove west germany, no longer a representable country
+# the trace variable will be used to hold all of the specifications for this plot.
+# some lines and variables come from stack overflow as noted.
 trace1 <- list(
   z = country.movie.data$passage.rate,
   autocolorscale = FALSE, 
   colorbar = list(title = "Passage Rates"), 
   colors = 'RdYlBu',
+  # passing in three letter country codes that can be read by plotly and turned into the world map.
+  # only includes countries that have data from bechdel test.
+  # the next couple lines of code are adapted from stack overflow
   locations =c("AUS","BRA","CMR", "CAN", "CHN", "COL", "CZE", "DNK", "FIN", "FRA", "DEU", "HKG", "IND", "IRL", 
                "ISR", "ITA", "JPN", "MEX", "NLD", "NZL", "NOR", "PER", "RUS", "ZAF", "KOR", "ESP", "TWN", "THA", "GBR", "USA"), 
   marker = list(line = list(
@@ -28,6 +35,7 @@ trace1 <- list(
   type = "choropleth", 
   zmin = 0
 )
+
 data <- list(trace1)
 layout <- list(
   geo = list(
